a0d564b13e4ff7575d78fe4b9369fb86833147d9
Deepak Raj <99645724+DeepakRajjj@users.noreply.github.com>
2024-07-22 08:42:07 +0530

    Update LoginPage.jsx

--- front/src/pages/LoginPage.jsx
+++ front/src/pages/LoginPage.jsx
@@ -10,7 +10,7 @@ const LoginPage = ({ onLogin }) => {
   const handleLogin = async (e) => {
     e.preventDefault();
     try {
-      const response = await axios.post('http://localhost:5000/api/auth/login', { email, password });
+      const response = await axios.post('https://doggypi-backend.onrender.com/api/auth/login', { email, password });
       localStorage.setItem('token', response.data.token); // Save token to local storage
       onLogin(); // Notify parent component about login success
     } catch (error) {
