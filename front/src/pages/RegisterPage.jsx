12220971caef9389e35ddf2a2c0f98e415d74d6b
Deepak Raj <99645724+DeepakRajjj@users.noreply.github.com>
2024-07-22 08:42:51 +0530

    Update RegisterPage.jsx

--- front/src/pages/RegisterPage.jsx
+++ front/src/pages/RegisterPage.jsx
@@ -10,7 +10,7 @@ const RegisterPage = () => {
   const handleRegister = async (e) => {
     e.preventDefault();
     try {
-      await axios.post('http://localhost:5000/api/auth/register', { email, password });
+      await axios.post('https://doggypi-backend.onrender.com/api/auth/register', { email, password });
       alert('Registration successful! You can now log in.');
     } catch (error) {
       setError('Error registering. Please try again.');
